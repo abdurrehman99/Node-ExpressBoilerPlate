4c2ec03123fad09e34ac61a58010bfed675c4741
Syed Abdur Rehman <syed.ebad1999@gmail.com>
2020-10-11 16:27:32 +0500

    env file & mongoDB setings

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  //DB URL
-  mongoURL: `mongodb+srv://syedebad:IXraoFyJXbhWUlYs@cluster0-xt32z.mongodb.net/FYP?retryWrites=true&w=majorityy`,
-
-  //Secret for JWT
-  secretOrKey: "Secret",
-};
--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const dotenv = require("dotenv");
 const path = require("path");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const logger = require("./middleware/logger");
 const cross = require("./middleware/cross");
-const dbURL = require("./config/keys").mongoURL;
-const User = require("./models/User");
 
 //Init app
 const app = express();
+
+//Init Express Router
+const router = express.Router();
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
 
 //Body Parser Middleware
 app.use(
@@ -40,33 +45,23 @@ app.use(helmet());
 // Prevent XSS attacks
 app.use(xss());
 
-app.post("/signup", (req, res) => {
-  const newUser = new User({
-    cnic: req.body.cnic,
-    email: req.body.email,
-    password: req.body.password,
-    mobile: req.body.mobile,
-  });
-  newUser
-    .save()
-    .then((user) => {
-      res.json(user);
-      console.log(user);
-    })
-    .catch((err) => res.send(err));
-  res.status(200).send("OK");
-});
+//Auth Route
+// router.use("/auth/user");
 
 //Default port for NODE app
 const PORT = process.env.PORT || 5000;
+
 //Connect to Mongo DB
 mongoose
-  .connect(dbURL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("MongoDB is connected"))
   .catch((error) => console.log(error));
 
 //Start Sever
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
